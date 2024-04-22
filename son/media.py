54c6d33bda9677663caa9eebeb898ff5acf06a56
le_woudar <rollandkev@yahoo.fr>
2024-04-22 21:45:30 +0200

    refactor(media): add a try/except clause when calling ffmpeg

--- son/media.py
+++ son/media.py
@@ -2,6 +2,8 @@ import re
 import subprocess
 from datetime import datetime, timedelta
 
+from son.console import error_console
+
 
 def get_seconds_from_str_time(str_time: str) -> float:
     """Converts a string time in format hh:mm:ss.ss into seconds"""
@@ -16,8 +18,14 @@ def get_seconds_from_str_time(str_time: str) -> float:
 
 
 def get_media_duration(filename: str) -> float:
-    result = subprocess.run(['ffmpeg', '-i', filename], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    try:
+        result = subprocess.run(['ffmpeg', '-i', filename], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, check=True)
+    except subprocess.CalledProcessError as e:
+        error_console.print(f'[error]unable to get media duration, reason:\n{e.stdout.decode()}')
+        raise SystemExit(1) from None
+
     output = result.stdout.decode()
     matches = re.search(r'Duration: (\d{2}:\d{2}:\d{2}.\d{2})', output)
-    if matches:
-        return get_seconds_from_str_time(matches.group(1))
+    # there should be no reason we don't parse the duration if the command executes successfully,
+    # so we don't add an "if" condition to check the matches
+    return get_seconds_from_str_time(matches.group(1))
